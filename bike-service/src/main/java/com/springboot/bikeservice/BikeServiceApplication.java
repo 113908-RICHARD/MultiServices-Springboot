46cbf032aacb5a98bf6cc19aebf5b0d9366b6a5f
genaro richard <grichard@encode.tech>
2024-06-11 09:51:38 -0300

    update

--- bike-service/src/main/java/com/springboot/bikeservice/BikeServiceApplication.java
+++ bike-service/src/main/java/com/springboot/bikeservice/BikeServiceApplication.java
@@ -3,7 +3,9 @@ package com.springboot.bikeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class BikeServiceApplication {
 
     public static void main(String[] args) {
