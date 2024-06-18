ced4dcbd471e97f9ab3d3edff062be0dc0050bb2
113908-RICHARD <128639135+113908-RICHARD@users.noreply.github.com>
2024-06-18 08:17:39 -0300

    update

--- gateway-service/src/main/java/com/springboot/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/springboot/gatewayservice/GatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.springboot.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 
