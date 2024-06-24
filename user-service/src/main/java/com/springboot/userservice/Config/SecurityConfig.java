8bf73c95aa2d38ffe32876553bafd774a6e5a984
genaro richard <grichard@encode.tech>
2024-06-24 12:29:02 -0300

    update

--- user-service/src/main/java/com/springboot/userservice/Config/SecurityConfig.java
+++ user-service/src/main/java/com/springboot/userservice/Config/SecurityConfig.java
@@ -2,20 +2,22 @@ package com.springboot.userservice.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  {
+public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
@@ -24,5 +26,7 @@ public class SecurityConfig  {
                         oauth2ResourceServer.jwt(withDefaults()))
 
                 .build();
+
+
     }
 }
